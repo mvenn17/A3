b969fdd302266e68f2e9efc0994e2cb8aa56c2d2
mvenn17 <mvenn@confederationcollege.ca>
2024-12-05 19:29:17 -0500

    saving because why not

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,22 @@
 <script>
+     import '../style.css';
      import Todo from './Todo.svelte';
+     import Confetti from './Confetti.svelte';
 </script>
 
 <h1>To Do App</h1>
 
 <Todo />
 
+<!-- confetti -->
+<div id="emitter"></div>
+<!-- confetti^^ -->
+
 <style>
+
+     h1 {
+          text-shadow: 1px 1px 2px #000000;
+          font-size: 3em;
+     }
      
 </style>
--- src/routes/Todo.svelte
+++ src/routes/Todo.svelte
@@ -7,8 +7,19 @@ function addItem() {
      if (todoItem == ''){
           return;
      }
-     todoList.push(todoItem);
+     todoList = [...todoList, {
+          text: todoItem,
+          done: false
+     }];
      todoItem = '';
+}
+
+function removeItem(index) {
+     todoList = todoList.toSpliced(index, 1);
+}
+
+function nuke (){
+     todoList = [];
 }
 
 $inspect(todoList);
@@ -19,13 +30,75 @@ $inspect(todoList);
 <button type="submit">Add</button>
 </form>
 
-<ul>
-     {#each todoList as item}
-          <li>{item}</li>
-     {/each}
-</ul>
+<div class="listContainer">
+     <ul>
+          {#each todoList as item, index}
+               <li>
+                    <input class="checkbox" type="checkbox" bind:checked={item.done}>
+                    <span class:done={item.done}>{item.text}</span>
+                    <button type="button" onclick={() => removeItem(index)}>x</button>
+               </li>
+          {/each}
+     </ul>
+</div>
 
 
+{#if (todoList.length == 0)}
+<button disabled type="button">Clear All</button>
+{:else}
+<button type="button" onclick={nuke}>Clear All</button>
+{/if}
 <style>
+.checkbox {
+     background-color: #daee77;
+     
+     width: 1.3em;
+    height: 1.3em;
+    background-color: white;
+    border-radius: 50%;
+    vertical-align: middle;
+    border: 1px solid #ddd;
+    appearance: none;
+    -webkit-appearance: none;
+    outline: none;
+    cursor: pointer;
+}
+.checkbox:checked {
+     background-color: #91f991bb;
+}
 
+     .listContainer {
+
+          display: flex;
+          align-items: center;
+          flex-direction: column;
+
+ /*          background-color: cornsilk;
+          color: black;
+          margin: 2% 40% 2%;
+          border-radius: 30px; */
+     }
+     ul{
+          list-style: none;
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+          padding: 0px;
+     }
+     li{
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          width: 100%;
+     }
+     li > span{
+          flex-grow: 5;
+          margin: 0px 10px;
+          display: flex;
+          justify-content: flex-start;
+     }
+     span.done {
+     color: grey;
+     text-decoration: line-through;
+     }
 </style>
